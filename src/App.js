8241f3225eb01dcb22700406119aab25817a13d1
Carl Cody <CarlCody34@gmail.com>
2019-07-16 10:13:26 -0400

    working on home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/Checkout';
 import About from './components/About';
+import Login from './components/Home'
 // import Navbar from './components/utility/Navbar';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='about' component={About} />
+        <Route exact path='about' component={Login} />
       </Switch>
     </Router>
   //  <div>
