0d599c7b82c9a1f72b0aac9b97a2ba0720ccca80
Carl Cody <CarlCody34@gmail.com>
2019-07-17 13:38:29 -0400

    need to finish homepage

--- server.js
+++ server.js
@@ -74,4 +74,109 @@ app.get('*', function (req, res) {
 // server.listen(port);
 server.listen( port , function() {console.log('Running on ' + app.get('port'));});
 
+
+// adding a eventListener for page load
+window.addEventListener("load", fetchBookmarks);
+
+// Find a form and add eventListener to it
+document.querySelector("form").addEventListener("submit",saveBookmark);
+
+// Saved Bookmarks
+function saveBookmark(event) {
+
+    // Prevent the page from reloading
+    event.preventDefault();
+
+   // Get site name and site url
+    var siteName = document.querySelector("#siteName").value;
+    var siteUrl = document.querySelector("#siteUrl").value;
+    // Creat a bookmark object
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+    // Check if local storage is empty also
+    if(localStorage.getItem("bookmarks") === null) {
+        // Init bookmarks array
+        var bookmarks =[];
+        // Adding first bookmark
+        bookmarks.push(bookmark);
+        // Adding or setting it to local storage
+        localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
+        // Else case means local storage is not empty
+    } else {
+        //Get current bookmarks from local storage
+        // String has to be transfered back to array JSON.parse is method
+        var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+        // Push new bookmark into bookmarks
+        bookmarks.push(bookmark);
+        //Once pushed going to send back to local storage
+        //set bookmarks to local storage
+        localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
+    }
+    // Reset the form
+    document.querySelector("form").reset();
+    //Fetch bookmarks 
+    fetchBookmarks();
+}
+function fetchBookmarks() {
+    // Get the bookmarks from local storage
+    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    // Select the bookmarks div
+    var output = document.querySelector("#bookmarks");
+    // Reset the bookmarks div
+    output.innerHTML = "";
+    // edit bookmarks one by one need a loop
+    // Loop over bookmarks
+    for (var i=0;i<bookmarks.length;i++) {
+        //Create a div
+        var div =document.createElement("div");
+        //Create a h3
+        var h3 = document.createElement("h3");
+        h3.textContent = bookmarks[i].name;
+       //Creat a  Visit Link
+        var a = document.createElement("a");
+        //
+        a.href = bookmarks[i].url;
+        a.className = "btn btn-success";
+        a.textContent = "Visit";
+       //Create a delete button
+       var button = document.createElement("button");
+       button.className = "btn btn-danger";
+       button.textContent = "Delete";
+      
+        button.addEventListener("click",function(event){
+            var name = event.target.parentElement.children[0].textContent;
+            deleteBookmark(name);
+        })
+     // append h3,a and button together into div
+        div.appendChild(h3);
+        div.appendChild(a);
+        div.appendChild(button);
+    // append div into output
+        output.appendChild(div);
+    }
+}
+// still have to a delete bookmark function
+function deleteBookmark(name) {
+
+    // Get bookmarks from localStorage
+var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+// Loop over bookmarks 
+for(var i=0; i<bookmarks.length;i++) {
+    //looking for bookmarks by given name
+
+    if(bookmarks[i].name === name) {
+        bookmarks.splice(i, 1);
+        break;
+    }
+}
+//Reset bookmarks back to localStorage
+localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
+//Re-fetch bookmarks output
+fetchBookmarks();
+}
+
+
  
---
+++ src/components/user/Profile.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <div>
+                <h1>navbar or title?</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-bottom">
+        <a class="navbar-brand" href="#">Hello Carl</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+<br>
+<h2><span class="text-warning">Buy</span> It Now Items</h2>
+<br>
+<div class="bd-example">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://steemitimages.com/DQmPqLUWjqxb8Q4WW992KDxuhHnK5bbDe1fA3U5vpd5U6YS/5.jpeg"
+                    class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Gold & SIlver</h5>
+                    <p>Great gift or inheritance items for friend or loved one.</p>
+                </div>
+            </div>
+            <div class="carousel-item active">
+                <div class="carousel-caption d-none d-md-block">
+                    <img src="https://justinteeantiques.com/wp-content/uploads/2018/11/massive-sterling-silver-jewellery-box-1.jpg"
+                        class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Coins & Collectibles</h5>
+                        <p>Great gift or inheritance items for friend or loved ones.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="https://steemitimages.com/DQmPqLUWjqxb8Q4WW992KDxuhHnK5bbDe1fA3U5vpd5U6YS/5.jpeg"
+                        class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Fashion & Apparel</h5>
+                        <p>For the person that likes to be different from everyone else.</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <h3><span class="text-warning">Items</span> Up For Auction</h3>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://steemitimages.com/DQmUDfNbw86cCPD6e4qftQYp554Hiy3G712D7msMAVp2r6G/9.jpeg"
+                    class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://steemitimages.com/DQmYZqD96LthPwjYBqyyxJnNzGzqVvug2UxbmjmDBTQwYYS/3.jpeg"
+                    class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <li class="list-group-horizontal">
+         <img src="https://historicalantiquesandcoins.com/wp-content/uploads/2017/08/q1-1.jpg"
+            class="rounded float-right w-50" alt="...">
+        <img src="https://foremansrarecoins.com/wp-content/uploads/2018/05/b847ab9cbb4bc46a569f48a23a13d5bb-150x149.jpg"
+            alt="..." class="img-thumbnail w-50">
+        <img src="https://justinteeantiques.com/wp-content/uploads/2018/11/massive-sterling-silver-jewellery-box-1.jpg"
+            alt="..." class="img-thumbnail w-50">
+         <img src="https://foremansrarecoins.com/wp-content/uploads/2018/05/b847ab9cbb4bc46a569f48a23a13d5bb-150x149.jpg"
+            alt="..." class="img-thumbnail w-50"> 
+        <img src="https://foremansrarecoins.com/wp-content/uploads/2018/09/IMG_2382-150x150.jpg"
+            class="rounded float-left w-50" alt="...">
+        <img src="https://www.umicore.de/storage/regiode/sized/pmm-7-max-600x0.jpg?1531190599"
+            class="rounded float-right w-50" alt="...">
+        <img src="https://www.smarterhobby.com/wp-content/uploads/2017/08/coin-collecting-guide.jpg"
+            class="rounded float-left w-50" alt="...">
+        <img src="https://steemitimages.com/640x0/https://steemitimages.com/DQmRiUu6WLHf6xhtNMovnwHF6t1CFX5bPdN2MLbr9LkQnCx/4.jpeg"
+            class="rounded float-right w-50" alt="...">
+        <img src="https://historicalantiquesandcoins.com/wp-content/uploads/2017/08/q1-1.jpg"
+            class="rounded float-right w-50" alt="...">
+    </li>
+
+    <span class="fixed-top">
+        <nav class="navbar navbar-light bg-dark">
+            <a class="navbar-brand" href="#">Home</a><a href="">Saved</a><a href="">Collectables & Art</a><a
+                href="">Home &
+                Garden</a><a href="">Silver & Gold</a>
+        </nav>
+    </span>
+    </body>
+
+            </div>
+        )
+    }
+}
